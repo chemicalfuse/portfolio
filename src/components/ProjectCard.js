9b2a9874ede99c3f6a9af59f012e387acfc53cff
Anand Keshri <anandkeshri271@gmail.com>
2025-05-31 12:12:13 +0400

    Update ProjectCard.js

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -3,7 +3,7 @@ import { Col } from "react-bootstrap";
 const ProjectCard = ({ url, description, imgUrl, title }) => {
   return (
     <Col size={12} sm={6} md={4}>
-      <a target="_blank" href="url">
+      <a target="_blank" href={url}>
         <div className="proj-imgbx">
           <img src={imgUrl} alt={title} />
           <div className="proj-txtx">
